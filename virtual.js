222ba864bbd8d8eec82d886c635a99ed7f57e210
pc2748 <pc2748@tc.columbia.edu>
2017-06-09 17:17:59 -0400

    Create virtual.js

--- virtual.js
+++ virtual.js
@@ -1,66 +1,60 @@
 //Virtual Teacher AI
-function italianTeacher() {
+
+function italianTeacher() { //This function creates the AI for the virtual Italian teacher.
   
   var italianConversation = document.getElementById("speakItalian").value; var response; //This code stores user input in a variable.
 
   if (italianConversation == "Ciao") {  //This is an if statement.  It determines what the virtual Italian teacher says.
-
   response = "Ciao, come stai?";
+  }
+
+  if (italianConversation == "ciao") {  //The virtual Italian teacher can respond to words typed different ways.
+  response = "Ciao, come stai?";        
+  }
+
+  if (italianConversation == "Bene") {  //The AI recognizes the Italian word for "well."
+  response = "Ottimo! Anch'io. Arrivederci.";
+  }
+
+
+  if (italianConversation == "bene") {
+  response = "Ottimo.  Anch'io. Arrivederci.";  //This response is given when the AI recognizes the Italian word for "well."
+  }
+
+  document.getElementById("italianResponse").innerHTML = response; //This code sends the AI response to the HTML.
+  }
+
+//Quiz 
+
+function correct() {  //This function outputs a response to a correct answer.
+  document.getElementById("quizoneresponse").innerHTML="Correct"; //This code displays the word "correct" in a specific element of the website.
 }
 
-if (italianConversation == "ciao") {
 
-response = "Ciao, come stai?";
-}
-
-if (italianConversation == "Bene") {
-
-response = "Ottimo! Anch'io. Arrivederci.";
-}
-
-
-if (italianConversation == "bene") {
-
-response = "Ottimo.  Anch'io. Arrivederci.";
-}
-
-document.getElementById("italianResponse").innerHTML = response;
-}
-
-//Quiz I
-function correct() {
-
-  document.getElementById("quizoneresponse").innerHTML="Correct";
-}
-
-function incorrect() {
-
-  document.getElementById("quizoneresponse").innerHTML="Incorrect";
+function incorrect() {  //This function outputs a response to an incorrect answer.
+  document.getElementById("quizoneresponse").innerHTML="Incorrect"; //This code displays the word "incorrect" in a specific element of the website.
 } 
 
 //Display Colors
-function colorDisplay() {
 
-document.querySelector("#rosso").style.color="red";
-
+function colorDisplay() { //This function turns an element red.
+  document.querySelector("#rosso").style.color="red"; //This code targets the element that will be changed red.
 }
 
-function colorDisplay2() {
-
-document.querySelector("#verde").style.color="green";
-
+function colorDisplay2() { //This function turns an element green.
+  document.querySelector("#verde").style.color="green"; //This code targets the element that will be changed green.
 }
 
-function colorDisplay3() {
-
-document.querySelector("#bianco").style.color="white";
-
+function colorDisplay3() { //This function turns an element white.
+  document.querySelector("#bianco").style.color="white"; //This code targets the element that will be changed white.
 }
 
 //Exit
-var name = prompt("What is your name?")
-function beforeYouGo() {
-document.write("Goodbye " + name + ". " + "The Condello School of Italian wants to thank you for using this free educational resource.  It is hoped that this website has helped you on your journey to becoming a fluent participant in the Italian language.  Please come back to visit us again.  We appreciate the time you have taken to study Italian with this Italian language school.");
+
+var name = prompt("What is your name?") //This code collects a user's name.
+
+function beforeYouGo() { //This code thanks the user by name for using the website.
+  document.write("Goodbye " + name + ". " + "The Condello School of Italian wants to thank you for using this free educational resource.  It is hoped that this website has helped you on your journey to becoming a fluent participant in the Italian language.  Please come back to visit us again.  We appreciate the time you have taken to study Italian with this Italian language school.");
 }
 
 
