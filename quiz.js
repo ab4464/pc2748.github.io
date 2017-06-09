914e03dde60902ca47d26f2fef97bb7e1e61ca90
pc2748 <pc2748@tc.columbia.edu>
2017-06-08 20:35:36 -0400

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,10 @@
+//Quiz Core
+function correct() {
+
+  document.getElementById("quizoneresponse").innerHTML="Correct";
+}
+
+function incorrect() {
+
+  document.getElementById("quizoneresponse").innerHTML="Incorrect";
+}
