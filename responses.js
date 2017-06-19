d3027205dd4686b466f76244d09284a1869faad8
pc2748 <pc2748@tc.columbia.edu>
2017-06-19 18:43:15 -0400

    Create responses.js

---
+++ responses.js
@@ -0,0 +1,12 @@
+//Responses for the Listening Practice app.
+
+var answerAnalyzer = [
+   {answerType1: ["You listen well.",
+                  "Your answer is similar to the correct one." 
+  ]
+},
+   {answerType2: [ "This is not correct.",
+                   "This is English."
+  ]                  
+}
+];
