f5682091cf072bc92229623bdbdd4c3895f144be
pc2748 <pc2748@tc.columbia.edu>
2017-06-19 18:42:11 -0400

    Update virtual.js

--- virtual.js
+++ virtual.js
@@ -83,3 +83,28 @@ function sentenceCreation (foodChoice) {
 } 
 
 
+//Listening Practice App
+
+function italianTeacher2() { //This function operates the Listening Practice app.
+  var italianConversation = document.getElementById("speakItalian2").value; var response; //This code stores user input in a variable.
+
+  if (italianConversation == "Come va?") {  //This is an if statement.  It determines the response that displays.
+    response = answerAnalyzer[0].answerType1[0];
+  }
+
+  if (italianConversation == "Come stai?") {  //The Listening Practice app can respond to words typed different ways.
+    response = answerAnalyzer[0].answerType1[1];        
+  }
+  
+  if (italianConversation == "Ciao") {  //The Listening Practice app recognizes this incorrect response.
+    response = answerAnalyzer[1].answerType2[0];
+  }
+
+  if (italianConversation == "How's it going?") {  //The app recognizes English.
+    response = answerAnalyzer[1].answerType2[1];  //This response is for an answer in English.
+  }
+  
+    document.getElementById("italianResponse").innerHTML = response; //This code sends the app response to the HTML.
+  }
+
+
