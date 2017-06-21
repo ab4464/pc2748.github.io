0d090a5012e79a4a710325b0bc1490ddbe5c2bac
pc2748 <pc2748@tc.columbia.edu>
2017-06-21 12:43:35 -0400

    Update virtual.js

--- virtual.js
+++ virtual.js
@@ -1,52 +1,52 @@
 //Virtual Teacher AI
-
 function italianTeacher() { //This function creates the AI for the virtual Italian teacher.
-  
-  var italianConversation = document.getElementById("speakItalian").value; var response; //This code stores user input in a variable.
 
-  if (italianConversation == "Ciao") {  //This is an if statement.  It determines what the virtual Italian teacher says.
-  response = "Ciao, come stai?";
+  var italianConversation = document.getElementById("speakItalian").value;
+  var response; //This code stores user input in a variable.
+
+  if (italianConversation == "Ciao") { //This is an if statement.  It determines what the virtual Italian teacher says.
+    response = "Ciao, come stai?";
   }
 
-  if (italianConversation == "ciao") {  //The virtual Italian teacher can respond to words typed different ways.
-  response = "Ciao, come stai?";        
+  if (italianConversation == "ciao") { //The virtual Italian teacher can respond to words typed different ways.
+    response = "Ciao, come stai?";
   }
 
-  if (italianConversation == "Bene") {  //The AI recognizes the Italian word for "well."
-  response = "Ottimo! Anch'io. Arrivederci.";
+  if (italianConversation == "Bene") { //The AI recognizes the Italian word for "well."
+    response = "Ottimo! Anch'io. Arrivederci.";
   }
 
 
   if (italianConversation == "bene") {
-  response = "Ottimo.  Anch'io. Arrivederci.";  //This response is given when the AI recognizes the Italian word for "well."
+    response = "Ottimo.  Anch'io. Arrivederci."; //This response is given when the AI recognizes the Italian word for "well."
   }
 
   document.getElementById("italianResponse").innerHTML = response; //This code sends the AI response to the HTML.
-  }
+}
 
 //Quiz 
 
-function correct() {  //This function outputs a response to a correct answer.
-  document.getElementById("quizoneresponse").innerHTML="Correct"; //This code displays the word "correct" in a specific element of the website.
+function correct() { //This function outputs a response to a correct answer.
+  document.getElementById("quizoneresponse").innerHTML = "Correct"; //This code displays the word "correct" in a specific element of the website.
 }
 
 
-function incorrect() {  //This function outputs a response to an incorrect answer.
-  document.getElementById("quizoneresponse").innerHTML="Incorrect"; //This code displays the word "incorrect" in a specific element of the website.
-} 
+function incorrect() { //This function outputs a response to an incorrect answer.
+  document.getElementById("quizoneresponse").innerHTML = "Incorrect"; //This code displays the word "incorrect" in a specific element of the website.
+}
 
 //Display Colors
 
 function colorDisplay() { //This function turns an element red.
-  document.querySelector("#rosso").style.color="red"; //This code targets the element that will be changed red.
+  document.querySelector("#rosso").style.color = "red"; //This code targets the element that will be changed red.
 }
 
 function colorDisplay2() { //This function turns an element green.
-  document.querySelector("#verde").style.color="green"; //This code targets the element that will be changed green.
+  document.querySelector("#verde").style.color = "green"; //This code targets the element that will be changed green.
 }
 
 function colorDisplay3() { //This function turns an element white.
-  document.querySelector("#bianco").style.color="white"; //This code targets the element that will be changed white.
+  document.querySelector("#bianco").style.color = "white"; //This code targets the element that will be changed white.
 }
 
 //Image Match JS Code
@@ -63,7 +63,7 @@ function wrongImage() {
 
 //Exit
 
-  var name = prompt("What is your name?") //This code collects a user's name.
+var name = prompt("What is your name?") //This code collects a user's name.
 
 function beforeYouGo() { //This code thanks the user by name for using the website.
   document.write("Goodbye " + name + ". " + "The Condello School of Italian wants to thank you for using this free educational resource.  It is hoped that this website has helped you on your journey to becoming a fluent participant in the Italian language.  Please come back to visit us again.  We appreciate the time you have taken to study Italian with this Italian language school.");
@@ -73,50 +73,51 @@ function beforeYouGo() { //This code thanks the user by name for using the websi
 //Sentence Creator JS Code
 
 //Create a function that accepts a food type and adds it to an Italian sentence.
-function sentenceCreation (foodChoice) { 
+function sentenceCreation(foodChoice) {
 
   //This variable accepts a food type.
   var foodChoice = document.getElementById("foodInput").value;
 
   //This code creates a sentence in Italian with the user input.
   document.getElementById("answerConnect").innerHTML = "La " + foodChoice + " è deliziosa.";
-} 
+}
 
 
 //Listening Practice App
 
 function italianTeacher2() { //This function operates the Listening Practice app.
-  var italianConversation2 = document.getElementById("speakItalian2").value; var response; //This code stores user input in a variable.
+  var italianConversation2 = document.getElementById("speakItalian2").value;
+  var response; //This code stores user input in a variable.
 
-  if (italianConversation2 == "Come va?") {  //This is an if statement.  It determines the response that displays.
+  if (italianConversation2 == "Come va?") { //This is an if statement.  It determines the response that displays.
     response = answerAnalyzer[0].answerType1[0];
   }
 
-  if (italianConversation2 == "Come stai?") {  //The Listening Practice app can respond to words typed different ways.
-    response = answerAnalyzer[0].answerType1[1];        
+  if (italianConversation2 == "Come stai?") { //The Listening Practice app can respond to words typed different ways.
+    response = answerAnalyzer[0].answerType1[1];
   }
-  
-  if (italianConversation2 == "Ciao") {  //The Listening Practice app recognizes this incorrect response.
+
+  if (italianConversation2 == "Ciao") { //The Listening Practice app recognizes this incorrect response.
     response = answerAnalyzer[1].answerType2[0];
   }
 
-  if (italianConversation2 == "How's it going?") {  //The app recognizes English.
-    response = answerAnalyzer[1].answerType2[1];  //This response is for an answer in English.
+  if (italianConversation2 == "How's it going?") { //The app recognizes English.
+    response = answerAnalyzer[1].answerType2[1]; //This response is for an answer in English.
   }
-  
-    document.getElementById("italianResponse2").innerHTML = response; //This code sends the app response to the HTML.
-  }
+
+  document.getElementById("italianResponse2").innerHTML = response; //This code sends the app response to the HTML.
+}
 
 //JS code for Fibonacci's Code app
-function activate()  {//Activate number analyzer.
- 
- //Retrieve number.
- var userInput = document.getElementById("receiver").value;
+function activate() { //Activate number analyzer.
 
- //Analyze number value with a for loop.
- for (i = userInput; i < 3; i++) {
- document.getElementById("rightNumber").innerHTML = "You got it, " + name + ".";
-   }
+  //Retrieve number.
+  var userInput = document.getElementById("receiver").value;
+
+  //Analyze number value with a for loop.
+  for (i = userInput; i < 3; i++) {
+    document.getElementById("rightNumber").innerHTML = "You got it, " + name + ".";
+  }
 }
 
 //Display Translations
@@ -126,10 +127,9 @@ function sentenceDisplay() { //This function displays the first translation.
 }
 
 function sentenceDisplay2() { //This function displays the second translation.
-  document.querySelector("#pizza").innerHTML="I want a pizza."; //This code targets the element that will be changed green.
+  document.querySelector("#pizza").innerHTML = "I want a pizza."; //This code targets the element that will be changed green.
 }
 
 function sentenceDisplay3() { //This function displays the third translation.
   document.querySelector("#gelato").innerHTML = " I like ice cream."; //This code targets the element that will be changed white.
 }
-
